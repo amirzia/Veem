9d3ceb0a3e5d5a60ac95deeae144df3caadf65ec
Amir <amir_zia@outlook.com>
2022-10-18 22:13:16 +0300

    Print input chars

---
+++ main.cc
@@ -0,0 +1,18 @@
+#include <ncurses.h>
+
+int main() {
+	int ichar; // input char
+
+	initscr(); // start curses mode
+	cbreak();
+	noecho();
+	
+	while (true) {
+		ichar = getch(); // wait for user input
+		printw("%c", ichar);
+		refresh(); // print the char on to the real screen
+	}
+	
+	endwin();
+	return 0;
+}
